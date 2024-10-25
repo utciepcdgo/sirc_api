69f77f37384f600e58a5e9c12d4225005dcce347
Alejandro Parra <alejandro.parra@iepcdurango.mx>
2024-10-25 01:40:48 -0600

    Modificaciones mínimas

--- app/Models/Registration.php
+++ app/Models/Registration.php
@@ -4,19 +4,25 @@ declare(strict_types=1);
 
 namespace App\Models;
 
+use Carbon\Traits\Date;
+use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 /**
+ * show off @property, @property-read, @property-write
+ *
  * @property int $id
  * @property string $name
  * @property string $first_name
  * @property string $second_name
- * @property array $placedate_birth
- * @property array $address_length_residence
+ * @property Json $placedate_birth Lugar y fecha de nacimiento.
+ * @property Json $address_length_residence Incluye la dirección y el tiempo de residencia.
  * @property string $occupation
- * @property string $voter_key
+ * @property string $voter_key Clave de Elector.
  * @property string $curp
+ * @property string $cic_code Código CIC.
+ * @property Date $expedition_date Fecha de expedición de la credencial de Elector.
  */
 class Registration extends Model
 {
@@ -31,13 +37,15 @@ class Registration extends Model
         'occupation',
         'voter_key',
         'curp',
+        'cic_code',
+        'expedition_date',
     );
 
     protected function casts(): array
     {
         return array(
-            'placedate_birth' => 'array',
-            'address_length_residence' => 'array',
+            'placedate_birth' => 'json',
+            'address_length_residence' => 'json',
         );
     }
 }
